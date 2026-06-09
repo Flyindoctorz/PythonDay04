0f631c843f255249d9ee3f1c9718c9d4e63cb060
cgelgon <cgelgon@f4r7s11.paris.42.school>
2026-06-09 14:57:07 +0200

    ex03

--- ex02/callLimit.py
+++ ex02/callLimit.py
@@ -1,9 +1,11 @@
 def callLimit(limit: int):
+    """wrap and return callLimiter"""
     count = 0
 
     def callLimiter(function):
-
+        """wrap and return limit_function"""
         def limit_function(*args: any, **kwds: any):
+            """return either the ft either error if limit reached"""
             nonlocal count
             if count < limit:
                 count += 1
---
+++ ex03/new_student.py
@@ -0,0 +1,22 @@
+import random
+import string
+from dataclasses import dataclass, field
+
+
+def generate_id() -> str:
+    return ("".join(random.choices(string.ascii_lowercase, k=15)))
+
+
+@dataclass
+class Student:
+    """A new student at 42"""
+    name: str
+    surname: str
+    active: bool = True
+    login: str = field(init=False)
+    # default_factory calls the ft, default just stores the method
+    id: str = field(init=False, default_factory=generate_id)
+
+    def __post_init__(self):
+        """init"""
+        self.login = self.name[0] + self.surname
